7793d28c6e6705905e8ab2867732c1461d8687fd
Your Name <ishiguro-tomoya@MacBook-Pro.local>
2020-08-27 20:26:08 +0900

    [Add]スライド機能

---
+++ paspol/js/script.js
@@ -0,0 +1,15 @@
+$(function() {
+	$(window).load(function(){
+		$('.flexslider').flexslider({
+			controlNav: false,
+			controlNav: false,
+			directionNav: false,
+			slideshow: true,
+			slideSpeedspeed: 100,
+			animation: "fade",
+			animationSpeed: 3000,
+			animationLoop: true
+		});
+
+	});
+});
